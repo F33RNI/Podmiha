c5566097db6a47d7aeacda914be81692a9779d70
XxOinvizioNxX <xxoinvizionxx@gmail.com>
2022-08-04 17:45:46 +0300

    New camera calibration with charuco

--- camera_calibration.py
+++ camera_calibration.py
@@ -20,79 +20,169 @@
 """
 
 import cv2
-import numpy as np
+from cv2 import aruco
 
-print("Enter camera ID: ")
-CAMERA_ID = int(input())
-print()
+# ChAruco board variables
+CHARUCOBOARD_ROWCOUNT = 7
+CHARUCOBOARD_COLCOUNT = 5
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_1000)
+
+# Create constants to be passed into OpenCV and Aruco methods
+CHARUCO_BOARD = aruco.CharucoBoard_create(
+    squaresX=CHARUCOBOARD_COLCOUNT,
+    squaresY=CHARUCOBOARD_ROWCOUNT,
+    squareLength=0.04,
+    markerLength=0.02,
+    dictionary=ARUCO_DICT)
+
+# Create the arrays and variables we'll use to store info like corners and IDs from images processed
+corners_all = []
+ids_all = []
+corners_all_old = []
+ids_all_old = []
+image_size = None
+
+CAMERA_ID = int(input("Enter camera ID: "))
 print("Camera ID:", CAMERA_ID)
 
 cap = cv2.VideoCapture(CAMERA_ID)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-WAIT_TIME = 10
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 1)
 
-# Termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# Show ChAruco Board
+cv2.imshow("ChAruco Board", CHARUCO_BOARD.draw((800, 800)))
 
-# Prepare object points
-obj_p = np.zeros((6 * 7, 3), np.float32)
-obj_p[:, :2] = np.transpose(np.mgrid[0:7, 0:6]).reshape(-1, 2)
-
-# Arrays to store object points and image points from all the images.
-object_points = []  # 3d point in real world space
-image_points = []  # 2d points in image plane.
-
-# Calibration images counter
+# Reset counter
 i = 0
 
+# Matrix and distortions
+camera_matrix = None
+camera_distortions = None
+
+# Print help info
+print("--------------")
+print("Show ChARuCo board on camera and press SPACE to apply calibration")
+print("Press X key to undo last calibration")
+print("--------------")
+
 while True:
-    # Read frame from camera
-    _, frame = cap.read()
+    # Get image from camera
+    ret, img = cap.read()
 
-    # operations on the frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
-
-    # If found, add object points, image points (after refining them)
-    if ret:
-        # Find corners
-        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-
-        # Draw detected chessboard
-        frame = cv2.drawChessboardCorners(frame, (7, 6), corners2, ret)
-
-        # Print help message
-        # print("Press SPACE to append calibration")
-
-        # Append calibration on Space bar press
-        # if cv2.waitKey(1) & 0xFF == 32:
-        # Append data
-        object_points.append(obj_p)
-        image_points.append(corners2)
-
-        # Print image number
-        i += 1
-        print("Calibration image: ", i)
-
-    # Show image
-    cv2.imshow("Frame", cv2.resize(frame, (640, 360)))
-
-    # Exit on Q key
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    # Check image
+    if not ret:
         break
 
-# Close camera and image preview
+    # Get key
+    key = cv2.waitKey(1) & 0xFF
+
+    # Grayscale the image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find aruco markers
+    corners, ids, _ = aruco.detectMarkers(
+        image=gray,
+        dictionary=ARUCO_DICT)
+
+    # Check for aruco
+    if ids is not None:
+        # Outline the aruco markers found
+        img = aruco.drawDetectedMarkers(
+            image=img,
+            corners=corners)
+
+        # Get charuco corners and ids from detected aruco markers
+        response, charuco_corners, charuco_ids = aruco.interpolateCornersCharuco(
+            markerCorners=corners,
+            markerIds=ids,
+            image=gray,
+            board=CHARUCO_BOARD)
+
+        # Undo on X
+        if key == ord("x"):
+            print("Undoing...")
+            # Restore old data
+            corners_all = corners_all_old.copy()
+            ids_all = ids_all_old.copy()
+
+            # Calculate camera_matrix and camera_distortions
+            print("Calculating calibration...")
+            calibration, camera_matrix, camera_distortions, rvecs, tvecs = aruco.calibrateCameraCharuco(
+                charucoCorners=corners_all,
+                charucoIds=ids_all,
+                board=CHARUCO_BOARD,
+                imageSize=image_size,
+                cameraMatrix=None,
+                distCoeffs=None)
+
+        # If a Charuco board was found, let's collect image/corner points
+        # Requiring at least 20 squares
+        if response > 20:
+            print("Charuco board detected. Press SPACE to append this image")
+
+            # Draw the Charuco board
+            img = aruco.drawDetectedCornersCharuco(
+                image=img,
+                charucoCorners=charuco_corners,
+                charucoIds=charuco_ids)
+
+            # Append calibration on Space bar press
+            if key == 32:
+                corners_all_old = corners_all.copy()
+                ids_all_old = ids_all.copy()
+                corners_all.append(charuco_corners)
+                ids_all.append(charuco_ids)
+
+                # Print image number
+                i += 1
+                print("Calibration image: ", i)
+
+                # Calculate camera_matrix and camera_distortions
+                print("Calculating calibration...")
+                calibration, camera_matrix, camera_distortions, rvecs, tvecs = aruco.calibrateCameraCharuco(
+                    charucoCorners=corners_all,
+                    charucoIds=ids_all,
+                    board=CHARUCO_BOARD,
+                    imageSize=image_size,
+                    cameraMatrix=None,
+                    distCoeffs=None)
+
+            # If our image size is unknown, set it now
+            if not image_size:
+                image_size = gray.shape[::-1]
+
+    # Show current image
+    cv2.imshow("Frame", cv2.resize(img, (640, 360)))
+
+    # Fix distortion in real-time
+    if camera_matrix is not None and camera_distortions is not None:
+        img_undistorted = cv2.undistort(img, camera_matrix, camera_distortions, None, camera_matrix)
+        cv2.imshow("Undistorted frame", cv2.resize(img_undistorted, (640, 360)))
+
+    # Exit on Q key
+    if key == ord("q"):
+        print("Exiting...")
+        break
+
+# Destroy any open CV windows and release camera
 cap.release()
 cv2.destroyAllWindows()
 
-# Calculate camera matrix and distortion coefficients
-_, mtx, dist, _, _ = cv2.calibrateCamera(object_points, image_points, gray.shape[::-1], None, None)
+# Calculate calibration
+print("Calculating calibration...")
+calibration, camera_matrix, camera_distortions, rvecs, tvecs = aruco.calibrateCameraCharuco(
+    charucoCorners=corners_all,
+    charucoIds=ids_all,
+    board=CHARUCO_BOARD,
+    imageSize=image_size,
+    cameraMatrix=None,
+    distCoeffs=None)
 
 # Save to file
+print("Writing to file...")
 cv_file = cv2.FileStorage("camera_calibration.yaml", cv2.FILE_STORAGE_WRITE)
-cv_file.write("camera_matrix", mtx)
-cv_file.write("dist_coeff", dist)
+cv_file.write("camera_matrix", camera_matrix)
+cv_file.write("dist_coeff", camera_distortions)
 cv_file.release()

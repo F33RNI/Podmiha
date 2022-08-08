6c36f66d33901a574a562633f0dc86cc51d524a7
XxOinvizioNxX <xxoinvizionxx@gmail.com>
2022-08-08 20:53:25 +0300

    Change version tag

--- Podmiha.py
+++ Podmiha.py
@@ -53,7 +53,7 @@ import winguiauto
 # https://gstreamer.freedesktop.org/data/pkg/windows/1.20.3/msvc/gstreamer-1.0-msvc-x86_64-1.20.3.msi
 
 
-APP_VERSION = "1.5.0"
+APP_VERSION = "1.5.2"
 
 SETTINGS_FILE = "settings.json"
 

27c5874be48058d96ef9fd85534fc2c7a534ad7b
XxOinvizioNxX <xxoinvizionxx@gmail.com>
2022-08-11 00:55:53 +0300

    Tampermonkey script updates

--- Podmiha-click-copy.user.js
+++ Podmiha-click-copy.user.js
@@ -1,50 +1,83 @@
 // ==UserScript==
 // @name         Podmiha-click-copy
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.2
 // @description  Podmiha tampermonkey script
 // @author       Fern Lane
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
-// Copy mode: 0 - entire element, 1 - word under cursor
-const COPY_MODE = 1;
+// Set to true to automatically remove the following characters from text when copying
+const REMOVE_CHARS = true;
+const CHARS_TO_REMOVE = ['?', '.'];
+
+// Set to true automatically remove spaces before and after selected text
+const TRIM = true;
+
+// Page custom CSS Style to make selection transparent
+const HTML_STYLE = "<style> " +
+    "::-moz-selection { \n" +
+    "\tcolor: inherit; \n" +
+    "\tbackground-color: transparent;\n" +
+    "\t}\n" +
+    "::selection { \n" +
+    "\tcolor: inherit;\n" +
+    "\tbackground-color: transparent; \n" +
+    "\t}" +
+    "</style>";
 
 
 (function() {
     'use strict';
 
-    // Mouse click on document
+    // Add custom CSS to the head of the page
+    document.getElementsByTagName('head')[0].innerHTML += HTML_STYLE;
+
+    // Mouse click on document -> copy word
     document.addEventListener('click', function(e) {
         e = e || window.event;
 
 		// Initialize text variable
         let textToCopy = "";
 
-        // Copy mode 1 - copy word under cursor
-        if (COPY_MODE === 1) {
-            try {
-                const word = getWordAtPoint(e.target, e.x, e.y);
-                if (word != null && word.length > 1) {
-                    textToCopy = word;
-                }
-            } catch (err) {
-                console.error('Error selecting word under cursor!', err);
+        // Get word under cursor
+        try {
+            const word = getWordAtPoint(e.target, e.x, e.y);
+            if (word != null && word.length > 1) {
+                textToCopy = word;
             }
+        } catch (err) {
+            console.error('Error selecting word under cursor!', err);
         }
 
-		// Copy mode 0 - copy entire element
-        else {
-            try {
-                const target = e.target || e.srcElement;
-                const text = target.textContent || target.innerText;
-                if (text != null && text.length > 1) {
-                    textToCopy = text;
-                }
-            } catch (err) {
-                console.error('Error selecting text from element!', err);
+		// Copy to clipboard
+        try {
+            if (textToCopy != null && textToCopy.length > 1) {
+                copyTextToClipboard(textToCopy);
+                //alert('Copied: ' + textToCopy);
             }
+        } catch (err) {
+            console.error('Error error copying text!', err);
+        }
+    }, false);
+
+    // Mouse double click -> copy entire element text
+    document.addEventListener('dblclick', function(e) {
+        e = e || window.event;
+
+		// Initialize text variable
+        let textToCopy = "";
+
+        // Get element text
+        try {
+            const target = e.target || e.srcElement;
+            const text = target.textContent || target.innerText;
+            if (text != null && text.length > 1) {
+                textToCopy = text;
+            }
+        } catch (err) {
+            console.error('Error selecting text from element!', err);
         }
 
 		// Copy to clipboard
@@ -62,6 +95,19 @@ const COPY_MODE = 1;
 	* Copies test to clipboard
 	*/
     function copyTextToClipboard(text) {
+        // Remove chars
+        if (REMOVE_CHARS) {
+            for (let i = 0; i < CHARS_TO_REMOVE.length; i++) {
+                text = text.replaceAll(CHARS_TO_REMOVE[i], '');
+            }
+        }
+
+        // Trim
+        if (TRIM) {
+            text = text.trim();
+        }
+
+        // Copy to clipboard
         if (!navigator.clipboard) {
             fallbackCopyTextToClipboard(text);
             return;
@@ -110,7 +156,7 @@ const COPY_MODE = 1;
 				range.setEnd(elem, currentPos+1);
 				if (range.getBoundingClientRect().left <= x && range.getBoundingClientRect().right >= x &&
 				range.getBoundingClientRect().top <= y && range.getBoundingClientRect().bottom >= y) {
-					range.expand("word");
+					range.expand('word');
                     const ret = range.toString();
                     range.detach();
 					return(ret);
